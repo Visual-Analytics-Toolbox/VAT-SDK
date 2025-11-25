68795c4f6c8afc8129447fe222d18959e3e36362
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2025-11-25 18:23:41 +0100

    chore: update sdk for robot pose

--- vaapi/types/log_status.py
+++ vaapi/types/log_status.py
@@ -86,6 +86,8 @@ class LogStatus(pydantic_v1.BaseModel):
 
     WhistlePercept: typing.Optional[int] = pydantic_v1.Field(default=None)
 
+    RobotPose: typing.Optional[int] = pydantic_v1.Field(default=None)
+
     #: IMUData
     IMUData: typing.Optional[int] = pydantic_v1.Field(default=None)
 
